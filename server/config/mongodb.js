272560f51ad42582fa721d00991408e4da791068
Anna sabbagh <afreenshabbagh@gmail.com>
2026-01-30 10:55:54 +0530

    first commit

---
+++ server/config/mongodb.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  console.log("MONGODB_URI =>", process.env.MONGODB_URI); // ✅ add this
+  mongoose.connection.on("connected", () => console.log("Database connected"));
+  await mongoose.connect(`${process.env.MONGODB_URI}/mern-auth`);
+};
+
+export default connectDB;
